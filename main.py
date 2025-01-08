276f1bbe63ca98d57f8448f09b29f272d0e264ed
sallyktish <ktishsally@gmail.com>
2025-01-08 23:10:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 !wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv 
 import pandas as pd
 
-df = pd.read_csv('parkinsons.csv')
+df =  pd.read_csv('parkinsons.csv')
 df.head()
 selected_features = ['DFA', 'D2']
 target_feature = 'status'
