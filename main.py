e4fa5d077b4157aa6498a7eece24779841804765
sallyktish <ktishsally@gmail.com>
2025-01-08 23:12:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 %autoreload 2
 
 # Download the data from your GitHub repository
-!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv 
+wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv 
 import pandas as pd
 
-df =  pd.read_csv('parkinsons.csv')
+df = pd.read_csv('parkinsons.csv')
 df.head()
 selected_features = ['DFA', 'D2']
 target_feature = 'status'
