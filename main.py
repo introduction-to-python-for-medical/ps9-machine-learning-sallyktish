970579939d287b90df13750145cfccc2d46afad0
sallyktish <ktishsally@gmail.com>
2025-01-08 20:44:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-€•)      sklearn.tree._classes”DecisionTreeClassifier”“”)”}”(	criterion”gini”splitter”best”	max_depth”Kmin_samples_split”Kmin_samples_leaf”Kmin_weight_fraction_leaf”G        max_features”Nmax_leaf_nodes”Nrandom_state”Nmin_impurity_decrease”G        class_weight”N	ccp_alpha”G        
-monotonic_cst”Nfeature_names_in_”joblib.numpy_pickle”NumpyArrayWrapper”“”)”}”(subclass”numpy” ndarray”“”shape”K…”order”C”dtype”hdtype”“”O8”‰ˆ‡”R”(K|”NNNJJK?t”b
-allow_mmap”‰numpy_array_alignment_bytes”Kub€cnumpy.core.multiarray
-_reconstruct
-q cnumpy
-ndarray
-qK …qc_codecs
-encode
-qX   bqX   latin1q†qRq ‡qRq	(KK…q
-cnumpy
-dtype
-qX   O8q‰ˆ‡q
-Rq(KX   |qNNNJJK?tqb‰]q(X   DFAqX   D2qetqb.•t       n_features_in_”K
-n_outputs_”Kclasses_”h)”}”(hhhK…”h h!h"h$i8”‰ˆ‡”R”(K<”NNNJJK t”bh*ˆh+Kub	               •       
-n_classes_”numpy.core.multiarray”scalar”“”h4C       ”†”R”
-max_features_”Ktree_”sklearn.tree._tree”Tree”“”Kh)”}”(hhhK…”h h!h"h4h*ˆh+Kub       •‹      K‡”R”}”(h	K
-node_count”Knodes”h)”}”(hhhK…”h h!h"h$V64”‰ˆ‡”R”(Kh(N(
-left_child”right_child” feature”	threshold”impurity”n_node_samples”weighted_n_node_samples”missing_go_to_left”t”}”(hQh$i8”‰ˆ‡”R”(Kh5NNNJJK t”bK †”hRh]K†”hSh]K†”hTh$f8”‰ˆ‡”R”(Kh5NNNJJK t”bK†”hUhdK †”hVh]K(†”hWhdK0†”hXh$u1”‰ˆ‡”R”(Kh(NNNJJK t”bK8†”uK@KKt”bh*ˆh+Kub                         `צ?rײ›w״?|             _@        ‏       ְ)O·ז?$             B@        ‏       ְ’׃|­׀?X             V@        •0       values”h)”}”(hhhKKK‡”h h!h"hdh*ˆh+Kub„B!„׀?¾ק{ן½ח?9ד8דא?ד8ד8?י¢‹.÷טֲ?F]tׁEכ?•        ub_sklearn_version”1.6.0”ub.
+%load_ext autoreload
+%autoreload 2
+
+# Download the data from your GitHub repository
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv 
+import pandas as pd
+
+df = pd.read_csv('parkinsons.csv')
+df.head()
+selected_features = ['DFA', 'D2']
+target_feature = 'status'
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+df[selected_features] = scaler.fit_transform(df[selected_features])
+from sklearn.model_selection import train_test_split
+
+X = df[selected_features]
+y = df[target_feature]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+from sklearn.tree import DecisionTreeClassifier
+
+model = DecisionTreeClassifier(max_depth=1)
+model.fit(X_train, y_train)
+from sklearn.metrics import accuracy_score
+accuracy_score(y, model.predict(X))
+print (accuracy_score(y, model.predict(X)))
+import joblib
+
+joblib.dump(model, 'my_model.joblib')
